f961dcc873f6e0d31143a43448844f7be6b03ed6
Fran López <fran@fran-lopez.com>
2013-11-25 20:34:21 -0300

    refactored initialization of map

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 var vger =  angular.module('vger', []);
 
-vger.controller('ListCtrl', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
-	
+vger.run(['$rootScope', function ($rootScope) {
 	// set visibility state for elements
-	$scope.loading = true;
-	$scope.messageVisible = false;
-	$scope.mapVisible = true;
-	$scope.spreadMenu = false;
+	$rootScope.loading = true;
+	$rootScope.messageVisible = false;
+	$rootScope.mapVisible = true;
+	$rootScope.spreadMenu = false;
 	
 	// set map on screen
-	var screenMap;
-	screenMap = L.map('map', {zoomControl: false}).locate({setView: true, maxZoom: 17});
-	L.control.zoom({position: 'bottomleft'}).addTo(screenMap);
+	$rootScope.screenMap = L.map('map', {zoomControl: false}).locate({setView: true, maxZoom: 17});
+	L.control.zoom({position: 'bottomleft'}).addTo($rootScope.screenMap);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(screenMap);
+	}).addTo($rootScope.screenMap);	
+}]);
 
+vger.controller('ListCtrl', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+	
 	var markIcon = L.icon({
 		iconUrl: 'mark.png',
 		shadowUrl: 'shadow.png',
@@ -41,7 +42,7 @@ vger.controller('ListCtrl', ['$rootScope', '$scope', '$http', function ($rootSco
 					ids += value.pageid + "|";
 					value.popUp = L.popup().setContent('<h4>' + value.title + '</h4>');
 					value.marker = L.marker([value.lat, value.lon], {icon: markIcon});
-					value.marker.addTo(screenMap).bindPopup(value.popUp);
+					value.marker.addTo($rootScope.screenMap).bindPopup(value.popUp);
 				});
 				ids = ids.slice(0, - 1);
 				// get thumbnails
@@ -64,13 +65,13 @@ vger.controller('ListCtrl', ['$rootScope', '$scope', '$http', function ($rootSco
 	
 	// load wikipedia entries for current location
 	$scope.reload = function() {
-		$rootScope.localization = screenMap.getCenter();
+		$rootScope.localization = $rootScope.screenMap.getCenter();
 		getWikipediaEntries($rootScope.localization.lat, $rootScope.localization.lng);
 	};
 	
 	// set map to current location
 	$scope.currentLocation = function() {
-		screenMap.locate({setView: true, zoom: 14, maxZoom: 17});
+		$rootScope.screenMap.locate({setView: true, zoom: 14, maxZoom: 17});
 	};
 
 	// open pop-up of a selected entry
@@ -91,12 +92,12 @@ vger.controller('ListCtrl', ['$rootScope', '$scope', '$http', function ($rootSco
 		var location = visitLocations[Math.floor(Math.random() * visitLocations.length)];
 		$scope.message = "Sorry, Vger couldn't find the current location... You've been redirected to "
 							+ location.name + ".";
-		screenMap.setView(location.position, 14);
-		$rootScope.localization = screenMap.getCenter();
+		$rootScope.screenMap.setView(location.position, 14);
+		$rootScope.localization = $rootScope.screenMap.getCenter();
 		getWikipediaEntries($rootScope.localization.lat, $rootScope.localization.lng);
 		$scope.messageVisible = true;
 	}
-	screenMap.on('locationerror', function() {
+	$rootScope.screenMap.on('locationerror', function() {
 		$scope.visitLocation();
 	});
 
@@ -110,16 +111,16 @@ vger.controller('ListCtrl', ['$rootScope', '$scope', '$http', function ($rootSco
 		shadowAnchor: [1, 48],  // the same for the shadow
 		popupAnchor:  [0, -52] // point from which the popup should open relative to the iconAnchor
 	});
-	screenMap.on('locationfound', function() {
-		$rootScope.localization = screenMap.getCenter();
+	$rootScope.screenMap.on('locationfound', function() {
+		$rootScope.localization = $rootScope.screenMap.getCenter();
 		// make the previous markers invisible
 		if ($scope.currentLocationMarkers.length > 0) {
 			$scope.currentLocationMarkers[$scope.currentLocationMarkers.length - 1].setOpacity(0);
 		}
 		var currentLocationMark = L.marker([$rootScope.localization.lat, $rootScope.localization.lng], {icon: personIcon});
-		currentLocationMark.addTo(screenMap);
+		currentLocationMark.addTo($rootScope.screenMap);
 		$scope.currentLocationMarkers.push(currentLocationMark);
-		screenMap.setZoom(14);
+		$rootScope.screenMap.setZoom(14);
 		getWikipediaEntries($rootScope.localization.lat, $rootScope.localization.lng);
 	});
 	
