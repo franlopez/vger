f835947dcad72903544e354846d338a74c48448f
fran <fran@franlopez.info>
2016-12-02 19:50:21 -0800

    start map

--- src/app/components/Vger.jsx
+++ src/app/components/Vger.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+const position = [51.505, -0.09];
 
 var Vger = React.createClass({
     render: function(){
-        return (
-            <div>
-                New Vger rewrite.
-            </div>
+        return(
+            <Map center={position} zoom={13} id='the-map'>
+                <TileLayer
+                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                    </Popup>
+                </Marker>
+            </Map>
         )
     }
 });
