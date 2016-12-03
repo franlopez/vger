2650419f60f1c10ef817d500aebd302316795ca1
fran <fran@franlopez.info>
2016-12-02 20:14:10 -0800

    use stamen toner-lite map tiles

--- src/app/components/Vger.jsx
+++ src/app/components/Vger.jsx
@@ -8,8 +8,8 @@ var Vger = React.createClass({
         return(
             <Map center={position} zoom={13} id='the-map'>
                 <TileLayer
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
+                    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
                 />
                 <Marker position={position}>
                     <Popup>
