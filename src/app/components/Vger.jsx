7681ab18c5ff02c6ac3a98a412266a4b2098a516
fran <fran@franlopez.info>
2016-12-02 19:32:40 -0800

    start react project

---
+++ src/app/components/Vger.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Vger = React.createClass({
+    render: function(){
+        return (
+            <div>
+                New Vger rewrite.
+            </div>
+        )
+    }
+});
+
+export default Vger;
---
+++ src/app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import Vger from './components/Vger.jsx';
+
+render(<Vger/>, document.getElementById('app'));
