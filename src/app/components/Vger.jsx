40e4d1affb037d1e2ddda6fc61212ea24a4956f7
fran <fran@franlopez.info>
2016-12-03 16:58:30 -0800

    put map on component

--- src/app/components/Vger.jsx
+++ src/app/components/Vger.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import Vmap from './Vmap.jsx';
 
 const position = [51.505, -0.09];
 
 var Vger = React.createClass({
     render: function(){
         return(
-            <Map center={position} zoom={13} id='the-map'>
-                <TileLayer
-                    url='http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
-                    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-                />
-                <Marker position={position}>
-                    <Popup>
-                        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-                    </Popup>
-                </Marker>
-            </Map>
+            <Vmap />
         )
     }
 });
---
+++ src/app/components/Vmap.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+const position = [51.505, -0.09];
+
+var Vmap = React.createClass({
+    render: function(){
+        return(
+            <Map center={position} zoom={13} id='vmap'>
+                <TileLayer
+                    url='http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
+                    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                    </Popup>
+                </Marker>
+            </Map>
+        )
+    }
+});
+
+export default Vmap;
