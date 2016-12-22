3b02e95714fd10c8a8510677986552bd73326e6b
fran <fran@franlopez.info>
2016-12-21 22:24:10 -0500

    use OSM tiles

--- src/app/components/Vmap.jsx
+++ src/app/components/Vmap.jsx
@@ -94,8 +94,8 @@ var Vmap = React.createClass({
                      bounds={[southWestBound, northEastBound]}
                      onMouseup={this.handleMoveend}>
                     <TileLayer
-                        url='https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'
-                        attribution='App by <a href="http://franlopez.info">Fran López</a>. Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.' />
+                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. App by <a href="http://franlopez.info">Fran López</a>.' />
                     {markers}
                 </Map>
             );
