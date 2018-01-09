576347663157621b1879207cb7fc7d2af7dbc730
franlopez <fran@franlopez.info>
2018-01-08 22:24:46 -0800

    move files and change paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/dist');
+var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src/app');
 
 var config = {
