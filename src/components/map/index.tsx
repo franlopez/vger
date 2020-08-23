837f2ef4f95755cdf814ad954fff2621dce5d0fb
Fran López <fran@franlopez.info>
2020-08-23 11:41:11 -0400

    desaturate map

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -23,6 +23,7 @@ function Map({ userLocation }: MapProps) {
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        className="tiles"
       />
 
       <Marker
